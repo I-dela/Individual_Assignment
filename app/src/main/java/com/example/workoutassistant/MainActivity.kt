f6248eb8bf30c5fcead6d8bfb01c9940bf3e152d
iddela <ilias.delawar@outlook.com>
2020-05-31 22:30:37 +0200

    bottom navigation done

--- app/src/main/java/com/example/workoutassistant/MainActivity.kt
+++ app/src/main/java/com/example/workoutassistant/MainActivity.kt
@@ -1,27 +1,49 @@
 package com.example.workoutassistant
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController : NavController
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+
+        bottomNav.setupWithNavController( navController)
+
+
+        val appBarConfiguration = AppBarConfiguration
+            .Builder(
+                R.id.iHome,
+                R.id.iWorkout
+            )
+            .build()
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+
+
+
     }
+
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -29,13 +51,19 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_settings -> true
+
             else -> super.onOptionsItemSelected(item)
         }
+
+
     }
+
+
 }
